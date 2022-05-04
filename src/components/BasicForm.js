acb7dd0f223931092690d06c1d973d122fe736cc
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-04 15:05:19 -0300

    Escribo el flujo de codigo del programa cuando interactuo con el input firstName para explicar que pasa y como se llaman reciprocamente las funciones y componente entre si

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -16,19 +16,43 @@
 //18. se retorna el formulario, el firstName recibe el form-control como clase, un value = '' y como firstNameHasError es false no se ejecuta el siguiente <p>
 
 //Flujo de codigo cuando modifico el input con un char (usamos de ejemplo lo referente al firstName):
+//1. escribo "c" en el input de First Name
+//2. se activa el onChange y apunto a firstNameChangeHandler 
+
+//RESUMEN de lo que se viene para entender 
+//pero no entro a useInput, porque ya tengo la funcion valueChangeHandler 
+//retornada en el objeto desestructurado (por eso leo el console.log del handler pero no el primero de useInput), por eso no necesito volver a entrar a 
+//useInput para ejecutarla, porque ya la tengo, y como el useReducer al que
+//apunta es global, entonces a pesar de que la funcion esta definida en useInput, yo la tengo disponible en el objeto desestructurado dentro del componente,
+//y desde el componente llamo al reducer (porque es global, repito), con dispatch, modifico el valor del firstName con el event.t.v que recibe por parametro,
+//y entonces como modifique un valor que afecta al comp BasicForm, se reevalua el componente y ahi si ejecuto el console.log del BasicForm y luego vuelvo a
+//llamar a useInput, y ahi si vuelvo a leer el console.log del useInput
+
+//..
+//7. BasicForm es reevaluado, y se vuelve a ejecutar useInput, pasando isNotEmpty como parametro apuntador de la funcion definida aca
+//..
+//9. ejecuto el console.log de isNotEmpty, veo el value que es el inputState.value, como es un char entonces es diferente de un string vacio, por lo cual
+//retorno TRUE
+//..
+//16. guardo los nuevos valores en el objeto desestructurado del useInput
+//17. ignoro el if porque solamente firstNameIsValid es TRUE
+//18. tomo la clase form-control porque a firstNameClass se le asigna el valor de firstNameHasError, que es false
+//19. retorno el jsx code con el value actualizado con el firstNameValue que viene con el char que modificamos en un principio y paso por toda la etapa de evaluacion
+//para validar o no el input y el form  
 
 
 import useInput from "../hooks/use-input";
 
 //funcion flecha que retorna TRUE si el el valor que llega por param (sin espacios, por eso el trim()) no esta vacio
 const isNotEmpty = (value) => {
-  console.log('El value de isNotEmpty: ', value)
+  console.log('El param-value de isNotEmpty: ', value)
   return value.trim() !== "";
 }; 
 
 const isEmail = (value) => value.includes("@"); //retorna TRUE siempre y cuando el string incluya @
 
 const BasicForm = (props) => {
+  console.log("El comp BasicForm ha sido reevaluado")
   const {
     value: firstNameValue,
     isValid: firstNameIsValid,
@@ -53,8 +77,6 @@ const BasicForm = (props) => {
     inputBlurHandler: emailBlurHandler,
     reset: resetEmail,
   } = useInput(isEmail);
-
-  console.log('linea43')
 
   let formIsValid = false;
 
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -11,6 +11,22 @@
 //12. y retorna 3 apuntadores de funciones Handlers y reset
 
 //Flujo de codigo cuando modifico el input con un char (usamos de ejemplo lo referente al firstName):
+//3. entro a valueChangeHandler, recibe el event.target.value de param, y activa el dispatch del reducer y le pasa un type y el event.t.v para el action
+//4. entro en el primer IF que es el que coincide con action.type, y retorno como value el action.value que no es otra cosa que el event.t.v,
+//ademas retorno el isTouched con el ultimo estado que haya quedado guardado el isTouched, que hasta que no se active el inputBlurHandler se va a mantener
+//en false
+//5. ahora inputState tiene a .value con el event.t.v, y a isTouched como false
+//6. como ejecute un hook, el useReducer, y los valores implicados estan en el comp BasicForm, se reevalua el componente BasicForm
+//..
+//8. se pasa por el console log de useInput (que me muestra el inputState.value actualizado con el char ingresado), se llama a validateValue que es 
+//isNotEmpty, que ahora la tengo disponible aca
+//..
+//10. me vuelve TRUE, y se guarda en valueIsValid, por lo cual hasError sera indefectiblemente false
+//11. ignoro los handlers definidos
+//12. retorno el value que es el inputState.value que es un char,
+//13. retorno el isValid que es el valueIsValid que es TRUE
+//14. retorno el hasError que es false
+//15. vuelvo a retornar las tres funciones como apuntadores
 
 
 import { useReducer } from 'react';
@@ -33,7 +49,7 @@ const inputStateReducer = (state, action) => { //recibe el action.type y el acti
   return inputStateReducer;
 };
 
-const useInput = (validateValue) => { //comp que se llama en BasicForm.js, recibe un param de validacion que es isNotEmpty || isEmail
+const useInput = (validateValue) => { //no es un comp, sino una funct que se llama en BasicForm.js, recibe un param de validacion que es isNotEmpty || isEmail
   
   const [inputState, dispatch] = useReducer(
     inputStateReducer, //primer parametro la funcion que modifica los valores del obj
@@ -42,20 +58,22 @@ const useInput = (validateValue) => { //comp que se llama en BasicForm.js, recib
   console.log('Estoy en el useInput: ', inputState.value)
 
   const valueIsValid = validateValue(inputState.value);
-  const hasError = !valueIsValid && inputState.isTouched;
+  const hasError = !valueIsValid && inputState.isTouched; //esta condicion compleja es para que solo muestre error si el usuario dejo de interactuar con el input aunque haya puesto algo incorrecto
 
-  console.log(valueIsValid)
+  /* console.log(valueIsValid) */
 
   const valueChangeHandler = (event) => {
-    console.log('el handler no fue ignorado')
+    console.log('el handler valueChangeHandler no fue ignorado: ', event.target.value)
     dispatch({ type: 'INPUT', value: event.target.value });
   };
 
   const inputBlurHandler = (event) => {
+    console.log('el handler inputBlurHandler no fue ignorado')
     dispatch({ type: 'BLUR' });
   };
 
   const reset = () => {
+    console.log('el handler reset no fue ignorado')
     dispatch({ type: 'RESET' });
   };
 
