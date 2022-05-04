a20f65967e06f91773f65b6aec38480966a0314a
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-04 12:08:38 -0300

    Agrego comentarios con el flujo de codigo durante el mount inicial del componente para explicar como funciona

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,7 +1,32 @@
-import useInput from '../hooks/use-input';
+//Flujo de codigo cuando se monta (usamos de ejemplo lo referente al firstName):
+//1. Se monta el componente BasicForm
+//2. Llamo a useInput y le paso por parametro isNotEmpty (que todavia NO es llamada, fijarse que se esta apuntando a esa funcion, porque no se pasa con parentesis)
+//..
+//5. ahora si isNotEmpty es llamada dentro de useInput, y viene con el parametro del inputState.value que inicialmente es un '', y devuelve false
+//..
+//13. trae los valores desestructurados que se guardan entre la linea 20 y 27, que son respectivamente:
+// '', 
+// false, 
+// false, 
+// (supongo, ya que son apuntadores aun no llamados) undefinedx3
+//14. inicializo formIsValid en false
+//15. no se entra en el IF porque todo es false
+//16. ignoro el submitHandler
+//17. se asigna false a firstNameClasses porque es lo que se retorno en firstNameHasError, entonces la class que se pasa es la de form-control
+//18. se retorna el formulario, el firstName recibe el form-control como clase, un value = '' y como firstNameHasError es false no se ejecuta el siguiente <p>
 
-const isNotEmpty = (value) => value.trim() !== '';
-const isEmail = (value) => value.includes('@');
+//Flujo de codigo cuando modifico el input con un char (usamos de ejemplo lo referente al firstName):
+
+
+import useInput from "../hooks/use-input";
+
+//funcion flecha que retorna TRUE si el el valor que llega por param (sin espacios, por eso el trim()) no esta vacio
+const isNotEmpty = (value) => {
+  console.log('El value de isNotEmpty: ', value)
+  return value.trim() !== "";
+}; 
+
+const isEmail = (value) => value.includes("@"); //retorna TRUE siempre y cuando el string incluya @
 
 const BasicForm = (props) => {
   const {
@@ -11,7 +36,7 @@ const BasicForm = (props) => {
     valueChangeHandler: firstNameChangeHandler,
     inputBlurHandler: firstNameBlurHandler,
     reset: resetFirstName,
-  } = useInput(isNotEmpty);
+  } = useInput(isNotEmpty); //paso por parametro la funcion que a su vez recibe por parametro el value desestructurado que viene de lo que retorna useInput
   const {
     value: lastNameValue,
     isValid: lastNameIsValid,
@@ -29,20 +54,22 @@ const BasicForm = (props) => {
     reset: resetEmail,
   } = useInput(isEmail);
 
+  console.log('linea43')
+
   let formIsValid = false;
 
   if (firstNameIsValid && lastNameIsValid && emailIsValid) {
     formIsValid = true;
   }
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
 
     if (!formIsValid) {
       return;
     }
 
-    console.log('Submitted!');
+    console.log("Submitted!");
     console.log(firstNameValue, lastNameValue, emailValue);
 
     resetFirstName();
@@ -50,48 +77,58 @@ const BasicForm = (props) => {
     resetEmail();
   };
 
-  const firstNameClasses = firstNameHasError ? 'form-control invalid' : 'form-control';
-  const lastNameClasses = lastNameHasError ? 'form-control invalid' : 'form-control';
-  const emailClasses = emailHasError ? 'form-control invalid' : 'form-control';
+  const firstNameClasses = firstNameHasError //lo que controla la condicion aca es lo que venga en firstNameHasError, porque su valor false || true se asigna a la const
+    ? "form-control invalid"
+    : "form-control";
+  const lastNameClasses = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
 
   return (
     <form onSubmit={submitHandler}>
-      <div className='control-group'>
+      <div className="control-group">
         <div className={firstNameClasses}>
-          <label htmlFor='name'>First Name</label>
+          <label htmlFor="name">First Name</label>
           <input
-            type='text'
-            id='name'
+            type="text"
+            id="name"
             value={firstNameValue}
             onChange={firstNameChangeHandler}
             onBlur={firstNameBlurHandler}
           />
-          {firstNameHasError && <p className="error-text">Please enter a first name.</p>}
+          {firstNameHasError && (
+            <p className="error-text">Please enter a first name.</p>
+          )}
         </div>
         <div className={lastNameClasses}>
-          <label htmlFor='name'>Last Name</label>
+          <label htmlFor="name">Last Name</label>
           <input
-            type='text'
-            id='name'
+            type="text"
+            id="name"
             value={lastNameValue}
             onChange={lastNameChangeHandler}
             onBlur={lastNameBlurHandler}
           />
-          {lastNameHasError && <p className="error-text">Please enter a last name.</p>}
+          {lastNameHasError && (
+            <p className="error-text">Please enter a last name.</p>
+          )}
         </div>
       </div>
       <div className={emailClasses}>
-        <label htmlFor='name'>E-Mail Address</label>
+        <label htmlFor="name">E-Mail Address</label>
         <input
-          type='text'
-          id='name'
+          type="text"
+          id="name"
           value={emailValue}
           onChange={emailChangeHandler}
           onBlur={emailBlurHandler}
         />
-        {emailHasError && <p className="error-text">Please enter a valid email address.</p>}
+        {emailHasError && (
+          <p className="error-text">Please enter a valid email address.</p>
+        )}
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,11 +1,26 @@
+//Flujo de codigo cuando se monta (usamos de ejemplo lo referente al firstName):
+//3. useInput es llamada, recibe de param una funcion flecha: isNotEmpty, llamada validateValue
+//4. validateValue aka isNotEmpty es llamada en la linea 33 y se le pasa de param el inputState.value, cuyo valor inicial es ''
+//..
+//6. valueIsValid es false, porque se retorno eso de isNotEmpty
+//7. hasError da false, porque no se cumple con que inputState.isTouched sea true, ya que inicialmente esta seteado en false
+//8. ignora los handlers, porque no estan siendo llamados todavia
+//9. retorna el value, seteado con el '' del inputState.value, 
+//10. retorna el isValid con el valueIsValid que en principio es false 
+//11. retorna el hasError que en principio va a ser false
+//12. y retorna 3 apuntadores de funciones Handlers y reset
+
+//Flujo de codigo cuando modifico el input con un char (usamos de ejemplo lo referente al firstName):
+
+
 import { useReducer } from 'react';
 
-const initialInputState = {
+const initialInputState = { //son los valores iniciales del obj del useReducer (inputState)
   value: '',
   isTouched: false,
 };
 
-const inputStateReducer = (state, action) => {
+const inputStateReducer = (state, action) => { //recibe el action.type y el action.value, lo que modifica es el value y el isTouched
   if (action.type === 'INPUT') {
     return { value: action.value, isTouched: state.isTouched };
   }
@@ -18,16 +33,21 @@ const inputStateReducer = (state, action) => {
   return inputStateReducer;
 };
 
-const useInput = (validateValue) => {
+const useInput = (validateValue) => { //comp que se llama en BasicForm.js, recibe un param de validacion que es isNotEmpty || isEmail
+  
   const [inputState, dispatch] = useReducer(
-    inputStateReducer,
-    initialInputState
+    inputStateReducer, //primer parametro la funcion que modifica los valores del obj
+    initialInputState //segundo param el objeto con los atributos y valores iniciales
   );
+  console.log('Estoy en el useInput: ', inputState.value)
 
   const valueIsValid = validateValue(inputState.value);
   const hasError = !valueIsValid && inputState.isTouched;
 
+  console.log(valueIsValid)
+
   const valueChangeHandler = (event) => {
+    console.log('el handler no fue ignorado')
     dispatch({ type: 'INPUT', value: event.target.value });
   };
 
@@ -39,7 +59,7 @@ const useInput = (validateValue) => {
     dispatch({ type: 'RESET' });
   };
 
-  return {
+  return { //retorna este obj con estos 6 atributos
     value: inputState.value,
     isValid: valueIsValid,
     hasError,
